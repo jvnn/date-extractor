affbe86d5286c44fed99a6f9b0971f0927541d73
Jussi Nieminen <nieminen.jussi@gmail.com>
2018-02-08 09:27:17 +0100

    allow ! and ? next to dates

--- test.sh
+++ test.sh
@@ -36,7 +36,7 @@ test "invalid dates" "30.2.2000 or Feb 30th 2000" "[]" 0
 test "date withouth year" "It's 20th January or December 31st" "[$this_year-01-20,$this_year-12-31]" 0
 test 'date with "of"' "4th of June this year was warmer than 28th of February 1910" "[$this_year-06-04,1910-02-28]" 0
 test 'date with "of" at the end' "This is the end: 6th of March" "[$this_year-03-06]" 0
-test "relative dates" "Tomorrow is in 2 days after yesterday or in a day, but not three weeks ago" "[$tomorrow,$in_two_days,$yesterday,$tomorrow,$three_weeks_ago]" 0
+test "relative dates" "Tomorrow!! is in 2 days after yesterday or in a day, but not three weeks ago?" "[$tomorrow,$in_two_days,$yesterday,$tomorrow,$three_weeks_ago]" 0
 test "timezone offset" "today" "[$yesterday]" 1440
 
 exit $retval
